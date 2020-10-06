8bd41621444b168420a1cd04583488ad52ff48dc
Paul Bui-Quang <paul.buiquang@rte-france.com>
2020-10-06 15:01:42 +0200

    Add user principal to security context
    Signed-off-by: Paul Bui-Quang <paul.buiquang@rte-france.com>

---
+++ afs-ws/afs-ws-server-utils/src/main/java/com/powsybl/afs/ws/server/utils/AfsSimpleSecurityContext.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+package com.powsybl.afs.ws.server.utils;
+
+import javax.ws.rs.core.SecurityContext;
+import java.security.Principal;
+
+/**
+ * @author Paul Bui-Quang <paul.buiquang at rte-france.com>
+ */
+public class AfsSimpleSecurityContext implements SecurityContext {
+
+    private Principal user;
+
+    public AfsSimpleSecurityContext(String username) {
+        this.user = new Principal() {
+            @Override
+            public String getName() {
+                return username;
+            }
+        };
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+        return user;
+    }
+
+    @Override
+    public boolean isUserInRole(String role) {
+        return false;
+    }
+
+    @Override
+    public boolean isSecure() {
+        return true;
+    }
+
+    @Override
+    public String getAuthenticationScheme() {
+        return null;
+    }
+}
