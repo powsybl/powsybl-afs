a1e39ae7b418c84a4e8625fba9ed2b6ff893b70b
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2021-02-18 17:51:43 +0100

    Springboot based implementation of AFS backend (#63)
    Adding a spring based implementation of AFS backend
    
    Co-authored-by: THIYAGARASA Pratheep Ext <thiyagarasapra@gm0winl319.bureau.si.interne>
    
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>
    Signed-off-by: THIYAGARASA Pratheep Ext <thiyagarasapra@gm0winl319.bureau.si.interne>

---
+++ afs-spring-server/src/main/java/com/powsybl/afs/server/AppDataWrapper.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.afs.server;
+
+import com.powsybl.afs.AppData;
+import com.powsybl.afs.AppFileSystem;
+import com.powsybl.afs.storage.AppStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Objects;
+
+/**
+ * Wrapper around {@link AppData} which provides additional checks around access to storage and filesystems.
+ *
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+@Component
+public class AppDataWrapper {
+
+    private final AppData appData;
+
+    @Autowired
+    public AppDataWrapper(AppData appData) {
+        this.appData = appData;
+    }
+
+    public AppData getAppData() {
+        return appData;
+    }
+
+    public AppStorage getStorage(String fileSystemName) {
+        AppStorage storage = appData.getRemotelyAccessibleStorage(fileSystemName);
+        if (storage == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "App file system '" + fileSystemName + "' not found");
+        }
+        return storage;
+    }
+
+    public AppFileSystem getFileSystem(String name) {
+        Objects.requireNonNull(appData);
+        Objects.requireNonNull(name);
+        AppFileSystem fileSystem = appData.getFileSystem(name);
+        if (fileSystem == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "App file system '" + name + "' not found");
+        }
+        return fileSystem;
+    }
+}
