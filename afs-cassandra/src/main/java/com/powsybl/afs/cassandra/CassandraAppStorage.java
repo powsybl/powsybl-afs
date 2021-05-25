daa26603b1ab714fd1f3fe7d83cc69ec7f292a82
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2021-05-25 09:19:28 +0200

    Fix data updated events in cassandra implementation
    In cassandra implementation, they were sent when starting to write the data,
    not when writing is actually done.
    
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- afs-cassandra/src/main/java/com/powsybl/afs/cassandra/CassandraAppStorage.java
+++ afs-cassandra/src/main/java/com/powsybl/afs/cassandra/CassandraAppStorage.java
@@ -1035,6 +1035,8 @@ public class CassandraAppStorage extends AbstractAppStorage {
             getSession().execute(insertInto(NODE_DATA_NAMES)
                     .value(ID, nodeUuid)
                     .value(NAME, name));
+
+            pushEvent(new NodeDataUpdated(nodeUuid.toString(), name), APPSTORAGE_NODE_TOPIC);
         }
     }
 
@@ -1044,7 +1046,6 @@ public class CassandraAppStorage extends AbstractAppStorage {
         Objects.requireNonNull(name);
         // flush buffer to keep change order
         changeBuffer.flush();
-        pushEvent(new NodeDataUpdated(nodeId, name), APPSTORAGE_NODE_TOPIC);
         return new BinaryDataOutputStream(nodeUuid, name);
     }
 
