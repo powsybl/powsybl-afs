e0369d7421b7e51676122446b0e0a4e8a0a43bdb
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2021-08-11 16:37:34 +0200

    minor fix
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- afs-cassandra/src/main/java/com/powsybl/afs/cassandra/NodeMetadataGetter.java
+++ afs-cassandra/src/main/java/com/powsybl/afs/cassandra/NodeMetadataGetter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -12,6 +12,8 @@ import java.util.Map;
 import java.util.function.Function;
 
 /**
+ * Helps exporting metadata to cassandra
+ *
  * @author Quentin CAPY <cappy.quentin at rte-france.com>
  */
 enum NodeMetadataGetter {
