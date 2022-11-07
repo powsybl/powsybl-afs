6627cc1f2dabc2898ee9e60c9d52d9cb14ae9a72
quentin <quentin.capy@rte-france.com>
2022-11-07 10:57:41 +0100

    update powsybl to 4.10.1
    Signed-off-by: quentin <quentin.capy@rte-france.com>

--- afs-security-analysis-local/src/test/java/com/powsybl/afs/security/local/SecurityAnalysisProviderMock.java
+++ afs-security-analysis-local/src/test/java/com/powsybl/afs/security/local/SecurityAnalysisProviderMock.java
@@ -7,12 +7,15 @@
 package com.powsybl.afs.security.local;
 
 import com.google.common.collect.ImmutableList;
+import com.powsybl.commons.reporter.Reporter;
 import com.powsybl.computation.ComputationManager;
 import com.powsybl.contingency.ContingenciesProvider;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.security.*;
+import com.powsybl.security.action.Action;
 import com.powsybl.security.interceptors.SecurityAnalysisInterceptor;
 import com.powsybl.security.monitor.StateMonitor;
+import com.powsybl.security.strategy.OperatorStrategy;
 
 import java.util.Collections;
 import java.util.List;
@@ -34,16 +37,24 @@ public class SecurityAnalysisProviderMock implements SecurityAnalysisProvider {
     }
 
     @Override
-    public CompletableFuture<SecurityAnalysisReport> run(Network network,
-                                                         String workingVariantId,
-                                                         LimitViolationDetector detector,
-                                                         LimitViolationFilter filter,
-                                                         ComputationManager computationManager,
-                                                         SecurityAnalysisParameters parameters,
-                                                         ContingenciesProvider contingenciesProvider,
-                                                         List<SecurityAnalysisInterceptor> interceptors,
-                                                         List<StateMonitor> monitors) {
-        LimitViolationsResult preContingencyResult = new LimitViolationsResult(true, ImmutableList.of(new LimitViolation("s1", LimitViolationType.HIGH_VOLTAGE, 400.0, 1f, 440.0)));
+    public CompletableFuture<SecurityAnalysisReport> run(
+            Network network,
+            String workingVariantId,
+            LimitViolationDetector detector,
+            LimitViolationFilter filter,
+            ComputationManager computationManager,
+            SecurityAnalysisParameters parameters,
+            ContingenciesProvider contingenciesProvider,
+            List<SecurityAnalysisInterceptor> interceptors,
+            List<OperatorStrategy> operatorStrategies,
+            List<Action> actions,
+            List<StateMonitor> monitors,
+            Reporter reporter) {
+        LimitViolationsResult preContingencyResult = new LimitViolationsResult(true,
+                ImmutableList.of(
+                        new LimitViolation("s1", LimitViolationType.HIGH_VOLTAGE, 400.0, 1f, 440.0)
+                )
+        );
         SecurityAnalysisResult result = new SecurityAnalysisResult(preContingencyResult, Collections.emptyList());
         return CompletableFuture.completedFuture(new SecurityAnalysisReport(result));
     }
