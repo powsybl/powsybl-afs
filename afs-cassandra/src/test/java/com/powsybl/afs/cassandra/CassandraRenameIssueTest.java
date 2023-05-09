ce5e1fe7568876e43b5ad2dcd6ebfca2275a39d6
GONCALVES Bruno <bruno.goncalves@rte-france.com>
2023-05-09 12:09:05 +0200

    Fix storage node rename to not delete children nodes with same names
    Signed-off-by: GONCALVES Bruno <bruno.goncalves@rte-france.com>
    (cherry picked from commit 9cd601ad1713a5b9dade2f156aff1d22c1d6fa50)

--- afs-cassandra/src/test/java/com/powsybl/afs/cassandra/CassandraRenameIssueTest.java
+++ afs-cassandra/src/test/java/com/powsybl/afs/cassandra/CassandraRenameIssueTest.java
@@ -10,7 +10,11 @@ import com.powsybl.afs.storage.AppStorage;
 import com.powsybl.afs.storage.NodeGenericMetadata;
 import com.powsybl.afs.storage.NodeInfo;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
@@ -31,5 +35,28 @@ public class CassandraRenameIssueTest {
         storage.flush();
         assertEquals(2, storage.getChildNodes(test1NodeInfo.getId()).size());
         assertEquals("newtest1", storage.getNodeInfo(test1NodeInfo.getId()).getName());
+    }
+
+    public void testRenameChildWithSameName(AppStorage storage) {
+        NodeInfo rootNodeId = storage.createRootNodeIfNotExists("test", "folder");
+        NodeInfo test1NodeInfo = storage.createNode(rootNodeId.getId(), "test1", "folder", "", 0, new NodeGenericMetadata());
+        storage.flush();
+        NodeInfo test2Project = storage.createNode(test1NodeInfo.getId(), "test2", "project", "", 0, new NodeGenericMetadata());
+        storage.setConsistent(test2Project.getId());
+        NodeInfo test2Folder = storage.createNode(test1NodeInfo.getId(), "test2", "folder", "", 0, new NodeGenericMetadata());
+        storage.setConsistent(test2Folder.getId());
+        storage.flush();
+        storage.renameNode(test2Project.getId(), "newTest2");
+        storage.flush();
+        List<NodeInfo> childNodes = storage.getChildNodes(test1NodeInfo.getId());
+        assertEquals(2, childNodes.size());
+        Optional<NodeInfo> first = childNodes.stream().filter(nodeInfo -> nodeInfo.getId().equals(test2Project.getId())).findFirst();
+        assertTrue(first.isPresent());
+        assertEquals("newTest2", first.get().getName());
+        Optional<NodeInfo> second = childNodes.stream().filter(nodeInfo -> nodeInfo.getId().equals(test2Folder.getId())).findFirst();
+        assertTrue(second.isPresent());
+        assertEquals("test2", second.get().getName());
+        assertEquals("test2", storage.getNodeInfo(test2Folder.getId()).getName());
+        assertEquals("newTest2", storage.getNodeInfo(test2Project.getId()).getName());
     }
 }
