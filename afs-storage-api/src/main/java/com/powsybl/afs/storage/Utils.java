95200b682178c2e77ebae8d0756c2d74fbe10dc0
Nicolas Rol <nicolas.rol@rte-france.com>
2025-02-24 17:58:07 +0100

    rework Utils.unzip method
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- afs-storage-api/src/main/java/com/powsybl/afs/storage/Utils.java
+++ afs-storage-api/src/main/java/com/powsybl/afs/storage/Utils.java
@@ -10,13 +10,21 @@ import com.google.common.io.ByteStreams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Enumeration;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 /**
@@ -27,6 +35,9 @@ import java.util.zip.ZipOutputStream;
 public final class Utils {
 
     private static final long MIN_DISK_SPACE_THRESHOLD = 10;
+    public static final int THRESHOLD_ENTRIES = 10000;
+    public static final long THRESHOLD_SIZE = 1000000000L; // 1 GB
+    public static final double THRESHOLD_RATIO = 10;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Utils.class);
 
@@ -90,21 +101,73 @@ public final class Utils {
      * @param nodeDir path to the directory where unzip
      */
     public static void unzip(Path zipPath, Path nodeDir) throws IOException {
-        try (ZipInputStream zis = new ZipInputStream(Files.newInputStream(zipPath))) {
-            Files.createDirectory(nodeDir);
-            ZipEntry entry = zis.getNextEntry();
-            while (entry != null) {
-                Path outputEntryPath = nodeDir.resolve(entry.getName());
-                if (entry.isDirectory() && !Files.exists(outputEntryPath)) {
-                    Files.createDirectory(outputEntryPath);
-                } else if (!entry.isDirectory()) {
-                    try (OutputStream fos = Files.newOutputStream(outputEntryPath)) {
-                        ByteStreams.copy(zis, fos);
+        unzip(zipPath, nodeDir, THRESHOLD_ENTRIES, THRESHOLD_SIZE, THRESHOLD_RATIO);
+    }
+
+    /**
+     * unzip
+     *
+     * @param zipPath zip Path
+     * @param nodeDir path to the directory where unzip
+     * @param thresholdEntries maximal number of entries
+     * @param thresholdSize maximal size of the zip file
+     * @param thresholdRatio maximal compression ratio
+     */
+    public static void unzip(Path zipPath, Path nodeDir, int thresholdEntries, long thresholdSize, double thresholdRatio) throws IOException {
+        try (ZipFile zipFile = new ZipFile(String.valueOf(zipPath.normalize()))) {
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+            AtomicInteger totalSizeArchive = new AtomicInteger(0);
+            int totalEntryArchive = 0;
+
+            while (entries.hasMoreElements()) {
+                ZipEntry ze = entries.nextElement();
+                Path outputEntryPath = nodeDir.resolve(ze.getName()).normalize();
+                totalEntryArchive++;
+
+                // Validation against Zip Slip
+                if (!outputEntryPath.startsWith(nodeDir)) {
+                    throw new IOException("Invalid path detected: " + ze.getName());
+                }
+
+                if (ze.isDirectory()) {
+                    Files.createDirectories(outputEntryPath);
+                } else {
+                    try (InputStream in = new BufferedInputStream(zipFile.getInputStream(ze));
+                        OutputStream out = new BufferedOutputStream(new FileOutputStream(String.valueOf(outputEntryPath)))) {
+
+                        copyZipEntry(ze, in, out, totalSizeArchive, thresholdSize, thresholdRatio);
                     }
                 }
-                entry = zis.getNextEntry();
+
+                if (totalEntryArchive > thresholdEntries) {
+                    // too many entries in this archive, can lead to inodes exhaustion of the system
+                    throw new IOException("Too many entries in this archive: " + zipPath.normalize().getFileName());
+                }
             }
-            zis.closeEntry();
+        }
+    }
+
+    private static void copyZipEntry(ZipEntry ze, InputStream in, OutputStream out, AtomicInteger totalSizeArchive, long thresholdSize, double thresholdRatio) throws IOException {
+        int nBytes;
+        byte[] buffer = new byte[2048];
+        int totalSizeEntry = 0;
+
+        while ((nBytes = in.read(buffer)) > 0) {
+            out.write(buffer, 0, nBytes);
+            totalSizeEntry += nBytes;
+            totalSizeArchive.getAndAdd(nBytes);
+
+            double compressionRatio = (double) totalSizeEntry / ze.getCompressedSize();
+            if (compressionRatio > thresholdRatio) {
+                // ratio between compressed and uncompressed data is highly suspicious, looks like a Zip Bomb Attack
+                throw new IOException("Ratio between compressed and uncompressed data is highly suspicious in: " + ze.getName());
+            }
+        }
+
+        if (totalSizeArchive.get() > thresholdSize) {
+            // the uncompressed data size is too much for the application resource capacity
+            throw new IOException("Uncompressed data size is too much for the application resource capacity in: " + ze.getName());
         }
     }
 
