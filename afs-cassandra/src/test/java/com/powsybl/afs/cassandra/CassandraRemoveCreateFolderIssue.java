331b105f21da7f862d91629d89b97f5093eef7fa
Nicolas Rol <nicolas.rol@rte-france.com>
2025-02-25 16:07:32 +0100

    Javadoc fix: add literal in mail addresses
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- afs-cassandra/src/test/java/com/powsybl/afs/cassandra/CassandraRemoveCreateFolderIssue.java
+++ afs-cassandra/src/test/java/com/powsybl/afs/cassandra/CassandraRemoveCreateFolderIssue.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 /**
- * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
  */
 public class CassandraRemoveCreateFolderIssue {
 
