16b7b7b2ee02d6f1284272548d25213e734aa370
Matthieu SAUR <matthieu.saur@rte-france.com>
2026-07-01 08:29:41 +0200

    Add logging capability to capture script output and enhance error handling
    Signed-off-by: Matthieu SAUR <matthieu.saur@rte-france.com>

--- afs-ext-base/src/main/java/com/powsybl/afs/ext/base/ScriptUtils.java
+++ afs-ext-base/src/main/java/com/powsybl/afs/ext/base/ScriptUtils.java
@@ -13,14 +13,19 @@ import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import groovy.lang.MissingMethodException;
 import groovy.lang.MissingPropertyException;
+import org.apache.commons.io.output.TeeWriter;
 import org.codehaus.groovy.control.CompilerConfiguration;
 import org.codehaus.groovy.control.MultipleCompilationErrorsException;
+import org.jspecify.annotations.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.UncheckedIOException;
+import java.io.Writer;
 import java.util.Map;
 
 /**
@@ -28,6 +33,7 @@ import java.util.Map;
  * @author Geoffroy Jamgotchian {@literal <geoffroy.jamgotchian at rte-france.com>}
  */
 public final class ScriptUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScriptUtils.class);
 
     private static final String SCRIPT_FILE_NAME = "test";
 
@@ -38,7 +44,9 @@ public final class ScriptUtils {
         String output = "";
         ScriptError error = null;
         Object value = null;
-        try (StringWriter outputWriter = new StringWriter()) {
+        StringWriter stringWriter = new StringWriter();
+        Writer loggerWriter = getLoggerWriter(contextObjects);
+        try (Writer outputWriter = new TeeWriter(stringWriter, loggerWriter)) {
             // put network in the binding so that it is accessible from the script
             Binding binding = new Binding();
             binding.setProperty("network", network);
@@ -50,8 +58,8 @@ public final class ScriptUtils {
             CompilerConfiguration config = new CompilerConfiguration();
             GroovyShell shell = new GroovyShell(binding, config);
             value = shell.evaluate(reader, SCRIPT_FILE_NAME);
-            outputWriter.flush();
-            output = outputWriter.toString();
+            stringWriter.flush();
+            output = stringWriter.toString();
         } catch (MultipleCompilationErrorsException e) {
             error = ScriptError.fromGroovyException(e);
         } catch (MissingPropertyException | MissingMethodException e) {
@@ -60,6 +68,11 @@ public final class ScriptUtils {
             throw new UncheckedIOException(e);
         }
         return new ScriptResult<>(value, output, error);
+    }
+
+    private static Writer getLoggerWriter(Map<Class<?>, Object> contextObjects) {
+        ScriptAppendOutputToLog scriptAppendOutputToLogConfig = (ScriptAppendOutputToLog) contextObjects.get(ScriptAppendOutputToLog.class);
+        return scriptAppendOutputToLogConfig != null ? new LoggerTraceWriter(scriptAppendOutputToLogConfig.appendOutputToLogger()) : null;
     }
 
     public static ScriptResult<Object> runScript(Network network, ScriptType scriptType, String scriptContent, Iterable<GroovyScriptExtension> extensions, Map<Class<?>, Object> contextObjects) {
@@ -71,6 +84,37 @@ public final class ScriptUtils {
             }
         } catch (IOException e) {
             throw new UncheckedIOException(e);
+        }
+    }
+
+    public record ScriptAppendOutputToLog(boolean appendOutputToLogger) {
+    }
+
+    private static final class LoggerTraceWriter extends Writer {
+        private final boolean appendOutputToLogger;
+
+        public LoggerTraceWriter(boolean appendOutputToLogger) {
+            this.appendOutputToLogger = appendOutputToLogger;
+
+        }
+
+        @Override
+        public void write(char @NonNull [] chars, int offset, int length) {
+            if (appendOutputToLogger) {
+                String log = String.valueOf(chars, offset, length);
+                log = log.replace(System.lineSeparator(), "");
+                ScriptUtils.LOGGER.info(log);
+            }
+        }
+
+        @Override
+        public void flush() {
+            // Do nothing
+        }
+
+        @Override
+        public void close() {
+            // Do nothing
         }
     }
 }
